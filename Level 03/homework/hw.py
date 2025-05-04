184050c5a0d0a0c90521c54af2dda334f5b99d04
data5792 <datakitiashvili16@gmail.com>
2025-05-04 20:25:43 +0400

    commit

---
+++ Level 03/homework/hw.py
@@ -0,0 +1,15 @@
+name=data
+lastname=kitiashvili
+age=13
+print (Hello,My name is Data and my last name is kitiashvili, i am 13 years old)
+number=10
+number2=15
+number3=30
+number4=20
+number5=5
+15-5
+30-20
+15+20
+10+5
+5+30
+15-10
