ab6218a1865dbeba864b15edfb69e8fb33fefec7
data5792 <datakitiashvili16@gmail.com>
2025-05-04 19:24:08 +0400

    commit

---
+++ Level 02/homework/hw.py
@@ -0,0 +1,13 @@
+# შექმენით 5-5 მაგალითი მათემატიკურ ოპერატორებზე +, -, * და 
+50-12
+info=13
+info2=data
+info3=kitiashvili
+info4=france
+info5=georgia
+# შექმენით 5 ცვლადი სადაც შეინახავთ თქვენზე ინფორმაციას და შემდეგ დაპრინტეთ თითოეული მათგანი
+print (13)
+print (data)
+print (kitiashvili)
+print (france)
+print (georgia)
