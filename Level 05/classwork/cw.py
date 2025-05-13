d10a05ac622c44844b0e0cf9743e3e563ad6bc18
data5792 <datakitiashvili16@gmail.com>
2025-05-13 22:13:38 +0400

    commit

--- Level 05/classwork/cw.py
+++ Level 05/classwork/cw.py
@@ -1,36 +1,28 @@
 #1) შემოატანინეთ მომხხმარებელს სტრინგი და გამოიტანეთ მისი პირველი ასო
-string1 = input("Enter a string: ")
-if string1:
-    print("The first character is:", string1[0])
-else:
-    print("The string is empty")
-
+string=input("enter a name: ")
+print (string[0])
 #2) შემოატანინეთ მომხმარებელს ორი სტრინგი და გამოიტანეთ მათ გაერთიანება
-str1 = input("Enter the first string: ")
-str2 = input("Enter the second string: ")
-print("Combined string:", str1 + str2)
-
+name=input("enter a name: ")
+name2=input("enter a second name: ")
+print (name + name2)
 #3) შემოატანინეთ მომხმარებელს სტრინგი და თუ მომხმარებლის შემოტანილი სტრინგი არის 'yes', მაშინ შემოატანინეთ სახელი და გამოიტანეთ ის. ყველა ვარიანტში საბოლლოდ დაემშვიდობეთ მომხმარებლლს მესიჯი 'goodbye' - თი
-answer = input("Do you want to continue? (yes/no): ")
-if answer.lower() == 'yes':
-    name = input("Enter your name: ")
-    print("Your name is:", name)
-
+guess=input("enter a yes or no: ")
+if guess=='yes':
+    name=input("enter a name: ")
+    print(name)
+    print("goodbye")
 #ყველა ვარიანტში საბოლლოდ დაემშვიდობეთ მომხმარებლლს მესიჯი 'goodbye' - თი
-print("goodbye")
+
 
 #4) შემოატანინეთ მომხმარებელს ასო თუ ეს ასო არის 'A' მაშინ გამოიტანეთ რიცხვი 100, თუ ის არის 'B' - 80, თუ 'C' - 60, თუ 'D' - 40, ხოლო თუ 'F' მაშინ სიტყვა 'Failed!'
-grade = input("Enter a grade letter (A, B, C, D, F): ").upper()
-
-if grade == 'A':
-    print(100)
-elif grade == 'B':
-    print(80)
-elif grade == 'C':
-    print(60)
-elif grade == 'D':
-    print(40)
-elif grade == 'F':
-    print("Failed!")
-else:
-    print("Invalid letter entered.")
+letter=input("enter a letter: ")
+if letter=='A':
+    print (100)
+if letter=='B':
+    print (80)
+if letter=='C':
+    print (60)
+if letter=='D':
+    print (40)
+if letter=='F':
+    print ('Falied!')
