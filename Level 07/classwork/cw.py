8ea007c2e010fff1c47085be8a57312690489225
data5792 <datakitiashvili16@gmail.com>
2025-05-20 22:22:40 +0400

    commit

---
+++ Level 07/classwork/cw.py
@@ -0,0 +1,16 @@
+#while true:
+#     print("Hello")
+#     print("Okay")
+#     print("bye")
+
+# ten=0
+# while ten<10:
+#     print("Hello")
+#     print("Okay")
+#     print("Bye")
+#     ten = ten + 1
+
+# number = 1
+# while number<100#:
+#     print("plus five")
+#     number = number + 5
---
+++ Level 07/homework/hw.py
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+while true:
+    user_password=input("enter ur password")
+    if user_password==python123:
+        break
+    print ("try again")
+
+
+
+
+  print ("acces granted")  
