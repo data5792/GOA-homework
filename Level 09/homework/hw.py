8049e3c3937e2cd9c0cf61d873edcfbc6e9a2b1e
data5792 <datakitiashvili16@gmail.com>
2025-05-25 16:28:48 +0000

    commit

--- Level 09/homework/hw.py
+++ Level 09/homework/hw.py
@@ -1,2 +1,12 @@
-# 2. ახსენით რა განსხვავებაა # 2. ახსენით რა განსხვავებაა implicit და explicit datatype comversion-ში.
-#implicit's ვაკეთებთ ხელით გამომყენებელები მაგრამ explicit's აკეთებს კომპიუტერი
+#2. ახსენით რა განსხვავებაა implicit და explicit datatype comversion-ში.
+#implicit  არის როცა კომპიუტერი თავისიდ აკეთებს 
+#explicit  არის როცა ჩვენ გამომყენებელი ვაკეთებთ
+# 4. შექმენით პროგრამა რომელიც მიიღებს მომხარებლისგან input-ს რიხვის სახით და დაბეჭდეთ ამ რიცხვამდე ყველა რიცხვი თანმიმდევრობით. (For loop)
+#end = int(input())
+#for i in range(0,end,1 ):
+#    print(i)
+# 5. გაალეთეთ მეოთხე დავალება While loop-ის გამოყენებით.
+#num = int(input("enter a number: "))
+#while num > 10:
+#    print (num)
+#    num += 1
