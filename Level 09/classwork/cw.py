c06cee45de6c857e971c181eec89d9e26e4155b9
data5792 <datakitiashvili16@gmail.com>
2025-05-25 17:14:24 +0400

    commit

---
+++ Level 09/classwork/cw.py
@@ -0,0 +1,25 @@
+# 1. მომხმარებელს შემოატანინეთ სამი რიცხვი, start/end/step და ჩასვით for loop-ში სათანადო ადგილას.
+# start = int(input("start number: "))
+# step = int(input("step number "))
+# end = int(input("end number: "))
+
+# for i in range (start, end, step):
+#     print (i)
+# start = int(input("num: "))
+
+# i = 0
+# while i != start:
+#     print (i)
+#     i += 1
+# 2. მომხმარებელს შემოატანინეთ რიცხვი და დაპრინტეთ რიცხვი ყოველ იტერაციაზე იქამდე, სანამ რიცხვი მომხმარებლის რიცხვს არ გაუტოლდება (While Loop)
+# x = int(input("enter your number: "))
+# i = 1
+# while i <= 10 :
+#     print (i)
+#     i += 1
+# 3. გააკეთეთ {2.} classwork For loop-ის გამოყენებით
+# x = int(input("Enter your number:"))
+#i = 1
+# while i <= 1:
+    # print(i)
+    # i+=1
