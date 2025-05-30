018b8afbc949568079c60fb261dd68cef34d88dd
data5792 <datakitiashvili16@gmail.com>
2025-05-30 19:25:57 +0000

    coomit

---
+++ Level 11/classwork/cw.py
@@ -0,0 +1,16 @@
+#1)მომხმარებელს შემოატანინეთ 3 რიცხვი და გამოიტანეთ მაგ სამი რიცხვის ჯამი 
+#num1=input("Enter a number: ")
+#num2=input("Enter a number: ")
+#num3=input("Enter a number: ")
+#print    (num1 + num2 + num3 )
+#2)დაბეჭდეთ რიცხვები 10 - 1 მდე 
+#i = 1
+#while i <= 10 :
+#    print (i)
+#    i += 1
+#3)დაბეჭდეთ 1- 100 რიცხვი მხოლოდ კენტები 
+#for i in range(1,101,1 ):
+#    print(i)
+#4)დაბეჭდეთ ყველა რიცხვი რომელიც 3 ზე უნაშთოდ იყოფა 1- 100
+#for i in range(0,101,3 ):
+#   print(i)
