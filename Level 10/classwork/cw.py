8869324c9732d8b778e9b6cada4c9848141d533a
data5792 <datakitiashvili16@gmail.com>
2025-06-06 16:39:34 +0000

    commit

--- Level 10/classwork/cw.py
+++ Level 10/classwork/cw.py
@@ -1,3 +1,23 @@
 #2) მომხმარებელს შემოატანინეთ რიცხვი და გამოიტანეთ ეს რიცხვი კენტია თუ ლუწი
+# number = int(input("Enter a number: "))
+
+# if number % 2 == 0:
+#     print("this number is even")
+# else:
+#     print("this number is odd")
 #3) მომხმარებელს შემოატანინეთ ორი რიცხვი და დაბეჭდეთ 'მეტია' თუ მეორე რიცხვი პირველზე მეტია თუ არადა დაბეჭდეთ  ''ნაკლებია"
+# a = int(input("Enter a number: "))
+# b = int(input("Enter another number: "))
+
+# if b > a:
+#     print("მეტია")
+# else:
+#     print("ნაკლებია")
 #4) მომხმარებელს შემოატანინეთ ორი რიცხვი და დაბეჭდეთ უფრო მაღალი რიცხვი
+number1= int(input("enter a number:  "))
+number2= int(input("enter a second number: "))
+
+if number1 > number2:
+    print(number1)
+else:
+    print(number2)
