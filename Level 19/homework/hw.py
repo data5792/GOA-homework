a4eea785a1f790f281be1832bd2b78753bc6ec8c
data5792 <datakitiashvili16@gmail.com>
2025-07-25 14:26:44 +0400

    commit

--- Level 19/homework/hw.py
+++ Level 19/homework/hw.py
@@ -3,28 +3,28 @@
 
 # numbers = [10, 20, 30, 40, 50, 60, 70]
 # აიღე ამ სიიდან პირველი 3 ელემენტი slicing-ის გამოყენებით.
-
+#print(number[0:3])
 #  2:
 # სია:
 
 # letters = ['a', 'b', 'c', 'd', 'e', 'f']
 # დაპრინტე სიიდან ელემენტები 'c', 'd', 'e' slicing-ით.
-
+#print(letters[2:5])
 #  3:
 # სია:
 
 # words = ['apple', 'banana', 'cherry', 'date', 'fig', 'grape']
 # აიღე სიის ბოლო 2 ელემენტი slicing-ის მეშვეობით.
-
+#print(words[4:6])
 #  4:
 # სია:
 
 # colors = ['red', 'green', 'blue', 'yellow', 'purple']
 # დაპრინტე სია შუიდან დაწყებული ბოლომდე (სადღაც 'blue'-დან ბოლომდე) slicing-ით.
-
+#print(colors[2:5])
 #  5:
 # სტრინგი:
 
-
 # text = "programming"
 # დაპრინტე მხოლოდ პირველი 5 სიმბოლო slicing-ის მეშვეობით.
+#print(text[0:6])
